3cc59ac2f59d600724a84270df0ad3bd347fa417
Jorge Pérez <jorge.perez.rojas@gmail.com>
2019-05-24 17:20:19 -0400

    data utils ges

--- models/src/ges_data_utils.py
+++ models/src/ges_data_utils.py
@@ -12,7 +12,6 @@ def collate_ges_tensor_data(data_list, binary=False):
 
     # Assume a list of pairs ((tensor_text, especialidad_idx, edad_idx), target) as input
     # Sort tensors by length and then stack them
-    #ipdb.set_trace()
     data_list.sort(key=lambda b: len(b[0][0]), reverse=True)
     X_seq, Y_seq = zip(*data_list)
     X_text_seq, X_especialidad_seq, X_edad_seq = zip(*X_seq)
@@ -27,11 +26,20 @@ def collate_ges_tensor_data(data_list, binary=False):
         Y = torch.LongTensor(Y_seq)
     return ((X_text, X_especialidad, X_edad, lengths), Y)
 
+def prepare_ges_batch_fn(batch, device):
+    (X,Y) = batch
+    (X_text, X_especialidad, X_edad, lengths) = X
+    X_text = X_text.to(device)
+    X_especialidad = X_especialidad.to(device)
+    X_edad = X_edad.to(device)
+    X = (X_text, X_especialidad, X_edad, lengths)
+    Y = Y.to(device)
+    return (X,Y)
 
 class GesDataset(Dataset):
     def __init__(self, csv_data_file, text_field='SOSPECHA_DIAGNOSTICA', 
                  ges_field='GES', ges_options=['True','False'], 
-                 especialidad_field='ESPECIALIDAD', unk_especialidad='None',
+                 especialidad_field='ESPECIALIDAD', unk_especialidad='<unk_esp>',
                  edad_field='EDAD', edad_limits=[1,4,5,6,7,9,15,20,25,35,50,55,60,61,65],
                  especialidad_voc=None, verbose=True,
                  **tagged_dataset_kwargs):
@@ -39,6 +47,9 @@ class GesDataset(Dataset):
         self.tagged_dataset = TaggedDataset(csv_data_file, tags=ges_options, 
             text_field=text_field, tag_prefix=ges_field, remove_duplicates=False, 
             make_proportions_equal=False, oversampling={}, verbose=verbose, **tagged_dataset_kwargs)
+
+        self.tags = self.tagged_dataset.tags
+        self.vocabulary = self.tagged_dataset.vocabulary
 
         if verbose:
             print('Reading edad and especialidad info...')
@@ -65,6 +76,7 @@ class GesDataset(Dataset):
             self.especialidad_voc = {w:i for i,w in enumerate(self._especialidad_indx_to_word)}
 
         self.converter = IntToOneHotVectorConverter(edad_limits)
+        self.edad_limits_size = len(edad_limits) + 1
         
     def __len__(self):
         return len(self.tagged_dataset.data)
@@ -77,6 +89,12 @@ class GesDataset(Dataset):
         edad_idx = self.converter.to_index(edad)
         output = ((text_tensor, especialidad_idx, edad_idx), cl)
         return output
+
+    def get_raw_data(self, idx):
+        text, clase = self.tagged_dataset.data[idx]
+        especialidad = self.especialidad_data[idx]
+        edad = self.edad_data[idx]
+        return ((text, especialidad, edad), clase)
 
 
 class IntToOneHotVectorConverter():
